0a04f4cd3863d9ffc5edff878513ed9ad56761fa
Andrey <andrey93-15@mail.ru>
2020-12-20 11:13:52 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty!!")]
+        [MinLength(10)]
+        public string Name { get; set; }
+
+        [Range(18,65,ErrorMessage = "Can be betwen 18 and 65")]
+        public int Age { get; set; }
+
+        public string City { get; set; }
+
+
+        public string Phone { get; set; }
+
+    }
+}
